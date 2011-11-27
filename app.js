71d906818b8622230377f34e1a35a046de420ccf
Adam Walters <ajwalters@gmail.com>
2011-11-27 16:17:54 -0600

    Working way of accomplishing broadcast

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var _ = require('underscore')
   , sockjs = require('sockjs')
   , sockjs_opts = {sockjs_url: "http://majek.github.com/sockjs-client/sockjs-latest.min.js"}
   , dir = __dirname
-  , sjs = new sockjs.Server(sockjs_opts)
+  , sjs = sockjs.createServer(sockjs_opts)
   , app_root = dir + '/app'
   , conf = {
          express: express
@@ -50,3 +50,42 @@ require('./config/routes.js').routes(conf);
 //Index resources
 app.listen(app.port);
 console.log("Pivotal Poker:\nlistening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Walters' playground
+console.log("--- Walters' Playground ---");
+
+app.sjs.on('connection', function(conn) {
+  console.log("sockjs: Received connection");
+  app.sjs.on('foo', function(){
+    console.log("sockjs: Replying to a foo command");
+    conn.write(["sockjs: received a foo event", conn]);
+  });
+  conn.on('data', function(data) {
+    // conn.write(message);
+    console.log("sockjs: Received message: ", data);
+    console.log("sockjs: foo value:", data.event);
+    if(data.event == "reply"){
+      conn.write("here's your stinking reply");
+    }
+  });
+});
+
+app.get("/test", function(req, res){
+  console.log(app.sjs);
+
+  app.sjs.emit("foo");
+  // app.sjs.emit("foo", { first : "parameter!" });
+  // app.sjs.on("connection", function(conn){
+
+  //   conn.on("data", function(message){
+  //     console.log("conn:", conn);
+  //     console.log("message:", message);
+  //   });
+
+  // });
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end("OK");
+});
+
